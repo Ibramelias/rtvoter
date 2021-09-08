f7bb559477fc7452fce7143fcf02b4962613f7e6
Ibrameias <ibramdarwish@gmail.com>
2021-09-08 19:57:56 -0400

    create loginUser function if the user success to login, this function weill redirect us to the memeber page

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var emailInput = $("input#email-input");
     var passwordInput = $("input#password-input");
 
-
+    // when the form is submitted, we validate there's an email and password entered.
     loginForm.on("submit", function(event){
         event.preventDefualt();
         var userData = {
@@ -17,12 +17,23 @@ $(document).ready(function(){
         }
 
         // if we have an email and password we run the loginUser function and clear the form. 
-
         loginUser(userData.email, userData.password);
         emailInput.val("");
         passwordInput.val("");
     });
 
-    
+    // loginUser does a post to our "api/login" route and if successful, redirects us to the member page.
+    function loginUser(email, password) {
+        $.post("api/login", {
+            email:email,
+            password:password
+        }).then(function(){
+            window.location.replace("/memebers");
+            // if there's an error, log the error
+        }).catch(function(err){
+            console.log(err)
+        })
+    }
+
 
 })
