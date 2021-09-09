fd163364a4dbdf3436b30aa7af075f40652bbd87
Ibrameias <ibramdarwish@gmail.com>
2021-09-08 20:28:28 -0400

    create handlelogin function to if there is any error

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -18,5 +18,27 @@ $(document).ready(function(){
         if(!userData.email || !userData.password){
             return;
         }
-    })
+
+        //if we have an email and password, run the signUp user function
+        signUpUser(userData.email, userData.password);
+        emailInput.val("");
+        passwordInput.val("");
+    });
+
+    // Does a post to the signup route. If successful, we are redirect to the members page
+    // Otherwise w log any errors
+
+    function signUpUser(email,password) {
+        $.post("/api/signup",{
+            email:email,
+            password:password
+        }).then(function(data){
+            window.location.replace("/members");
+        }).catch(handleLoginError);
+    }
+
+    function handleLoginError(err){
+        $("#alart .msg").text(err.responseJSON);
+        $("#alart").fadeIn(500);
+    }
 })
