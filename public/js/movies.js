5eb471862bba64d7764eca9304beeb288daaff5e
Ibrameias <ibramdarwish@gmail.com>
2021-11-25 21:38:10 -0500

    start to append date in the page

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,28 +1,24 @@
 
-
 $(document).ready(function () {
-
-    // var movie;
-    // var queryurl;
-    // var Apikey = "c9999e90";
-
-    // "&apikey=trilogy"
-    // "https://www.omdbapi.com/?t=" + movie + "&api_key=c9999e90";
-
-    // var film = $("#films");
 
     $("#btn").on("click", function getMovie() {
 
         var movie = $("#movie").val();
         var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
-
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
+            console.log(response);
 
-            $("#films").text(JSON.stringify(response));
+            
+            var name = response.Title;
+           
+            var movie1 = $("<p>").text(name);
+            // console.log(movie1);
+            $("#films").append(movie1);
+
         })
 
 
