6ea85d7e116bf02f6b42ddff7c56b5529013b907
Ibrameias <ibramdarwish@gmail.com>
2021-11-25 20:45:51 -0500

    start to get response from api, now  working on to append title , rate and poster for movies

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,9 +1,33 @@
 
-var movie;
-var queryurl;
-var Apikey = "c9999e90";
 
-function getMovie (){
+$(document).ready(function () {
 
-    queryurl = ""
-}
+    // var movie;
+    // var queryurl;
+    // var Apikey = "c9999e90";
+
+    // "&apikey=trilogy"
+    // "https://www.omdbapi.com/?t=" + movie + "&api_key=c9999e90";
+
+    // var film = $("#films");
+
+    $("#btn").on("click", function getMovie() {
+
+        var movie = $("#movie").val();
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            $("#films").text(JSON.stringify(response));
+        })
+
+
+
+    })
+
+
+})
