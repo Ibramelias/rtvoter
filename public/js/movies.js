fa0499240c82bfd79f74ec96fe938d76f1fa4495
Ibrameias <ibramdarwish@gmail.com>
2021-11-27 15:09:20 -0500

    create map() to display movie name and year from array

--- public/js/movies.js
+++ public/js/movies.js
@@ -11,24 +11,36 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            
+
 
             // var name = response.Title;
             // var movie1 = $("<p>").text(name);
             // $("#films").append(movie1);
-            
-            
+
+
             // $("#films").text(JSON.stringify(data[i].Title));
 
 
-            for (var i = 0; i <response; i++) {
+            // for (var i = 0; i <response.lenght; i++) {
 
-                var name = response.Search[i];
-                var movie1 = $("<p>").text(name);
-                console.log(movie1);
+            //     var name = response.Search[i];
+            //     var movie1 = $("<p>").text(name);
+            //     console.log(movie1);
+
+
+            // }
+
+            let movieInfo = response.Search.map(function (element) {
+                return `${element.Title} ${element.Year}`;
+
+
+                // var name = data.Title;
+                // var movie1 = $("<p>").text(name);
                
+            })
+            console.log(movieInfo);
+            $("#films").append(movieInfo[0]);
 
-            }
 
             // var imgURL = data[0].Poster;
             // var img = $("<img>").attr("src",imgURL );
