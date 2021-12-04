e299d434598504e66971c75135b7f3acddc7cab3
Ibrameias <ibramdarwish@gmail.com>
2021-12-03 19:06:59 -0500

    finalaize the first section of movies

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,94 +1,44 @@
 
 $(document).ready(function () {
 
-    $("#btn").on("click", function getMovie() {
 
-        // var movie = $("#movie").val();
-        var queryURL = "https://www.omdbapi.com/?s=life&apikey=trilogy";
+     // var movie = $("#movie").val();
+     var queryURL = "https://www.omdbapi.com/?s=life&apikey=trilogy";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-
-
-            // var name = response.Title;
-            // var movie1 = $("<p>").text(name);
-            // $("#films").append(movie1);
+     $.ajax({
+         url: queryURL,
+         method: "GET"
+     }).then(function (response) {
+         console.log(response);
 
 
-            // $("#films").text(JSON.stringify(data[i].Title));
+        //  let title = response.Search.map(function (element) {
+        //      return `${element.Title}`;
+
+        //  })
+
+         let Poster = response.Search.map(function (element) {
+             return `${element.Poster}`;
+
+         })
+
+         console.log(Poster[3]);
+
+         for (var i = 0; i < Poster.length; i++) {
+            //  console.log(title[i])
+            //  var name = $("<p>").text(title[i]);
+             var img = $("<img>").attr("src", Poster[i]);
 
 
-            // for (var i = 0; i <response.lenght; i++) {
+             $("#films").addClass("info")
+             $("#films").append(img);
 
-            //     var name = response.Search[i];
-            //     var movie1 = $("<p>").text(name);
-            //     console.log(movie1);
+           
+         }
 
+ 
 
-            // }
-
-            let title = response.Search.map(function (element) {
-                return `${element.Title}`;
-
-            })
-
-            let Poster = response.Search.map(function (element) {
-                return `${element.Poster}`;
-
-            })
-
-            console.log(Poster[3]);
-
-            for (var i = 0; i < title.length; i++) {
-                console.log(title[i])
-                var name = $("<p>").text(title[i]);
-                var img = $("<img>").attr("src", Poster[i]);
-
-
-                $("#films").addClass("info")
-                $("#films").append(name,img);
-
-                // for (var j = 0; j < Poster.length; j++){
-
-                //    
-                //     $("#films").append(img);
-                    
-                // }
-              
-            }
-
-       
-
-            // var imgURL = data[0].Poster;
-
-
-            // console.log(img)
-
-
-
-
-            // var imgURL = data[0].Poster;
-            // var img = $("<img>").attr("src",imgURL );
-            // $("#films").append(img);
-
-            // var name = data.Title;
-            // var movie1 = $("<p>").text(name);
-            // $("#films").append(movie1);
-
-            // var year = data.Released;
-            // var release = $("<p>").text("Released on " + year );
-            // $("#films").append(release);
-
-        })
-
-
-
-
-
-    })
+     })
 
 
 })
