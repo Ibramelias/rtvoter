41c3af72c41a84b6c551fcbad4936de332f419be
Ibrameias <ibramdarwish@gmail.com>
2021-12-05 11:43:17 -0500

    wotking on make the code reuseble to used that in many sections

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,44 +1,72 @@
 
+var category;
+// var categoryTwo = "funny";
+// var categoryThree = "sad";
+// var categoryFour = "happy";
+// var categoryFive = "gold";
+
+
+
+
 $(document).ready(function () {
 
+    function getResult() {
 
-     // var movie = $("#movie").val();
-     var queryURL = "https://www.omdbapi.com/?s=life&apikey=trilogy";
+        var queryURL = "https://www.omdbapi.com/?s=" + category + "&apikey=trilogy";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
 
-     $.ajax({
-         url: queryURL,
-         method: "GET"
-     }).then(function (response) {
-         console.log(response);
+            //  let title = response.Search.map(function (element) {
+            //      return `${element.Title}`;
+
+            //  })
+
+            // crreate a map function to loop in every result and get movie poster from data response //  
+            let Poster = response.Search.map(function (element) {
+                return `${element.Poster}`;
+            })
+
+            // console.log(Poster[3]);
+
+        })
+
+    } // function (getResults)
+    getResult();
+
+    
 
 
-        //  let title = response.Search.map(function (element) {
-        //      return `${element.Title}`;
+    // var movie = $("#movie").val();
 
-        //  })
-
-         let Poster = response.Search.map(function (element) {
-             return `${element.Poster}`;
-
-         })
-
-         console.log(Poster[3]);
-
-         for (var i = 0; i < Poster.length; i++) {
-            //  console.log(title[i])
-            //  var name = $("<p>").text(title[i]);
-             var img = $("<img>").attr("src", Poster[i]);
-
-
-             $("#films").addClass("info")
-             $("#films").append(img);
-
-           
-         }
-
- 
-
-     })
 
 
 })
+
+
+
+
+
+
+// for (var i = 0; i < Poster.length; i++) {
+//     //  console.log(title[i])
+//     //  var name = $("<p>").text(title[i]);
+//     var img = $("<img>").attr("src", Poster[i]);
+
+
+//     $("#categoryOne").addClass("info")
+//     $("#categoryOne").append(img);
+
+
+// }
+
+
+
+
+// $("#two").click(function(){
+//     category = 'sad';
+//     getResult();
+
+// })
