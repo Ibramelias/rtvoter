a809d857b8776cda54786f870841928a5ad7de69
Ibrameias <ibramdarwish@gmail.com>
2021-12-05 13:02:45 -0500

    rying to figuer out how to append every category in diffrent section

--- public/js/movies.js
+++ public/js/movies.js
@@ -5,38 +5,45 @@ var category;
 // var categoryFour = "happy";
 // var categoryFive = "gold";
 
-
-
-
 $(document).ready(function () {
 
     function getResult() {
-
+        // category = 'life';
         var queryURL = "https://www.omdbapi.com/?s=" + category + "&apikey=trilogy";
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
-
-            //  let title = response.Search.map(function (element) {
-            //      return `${element.Title}`;
-
-            //  })
-
+            // console.log(response);
             // crreate a map function to loop in every result and get movie poster from data response //  
             let Poster = response.Search.map(function (element) {
                 return `${element.Poster}`;
             })
-
-            // console.log(Poster[3]);
-
+            for (var i = 0; i < Poster.length; i++) {
+                //  console.log(title[i])
+                //  var name = $("<p>").text(title[i]);
+                var img = $("<img>").attr("src", Poster[i]);
+                $("#categoryOne, #categoryTwo, #categoryThree, #categoryFour, #categoryFive").addClass("info")
+                $("#categoryOne").append(img);
+            }
         })
-
     } // function (getResults)
-    getResult();
+   
+    $("#one").append(function () {
+        category = "life";
+        // $("#categoryOne").append();
+        getResult();
+        
+    })
 
-    
+
+    $("#two").append(function () {
+        category = "happy";
+        // $("#categoryTwo").append(getResult(category));
+        getResult();
+        
+    })
+
 
 
     // var movie = $("#movie").val();
