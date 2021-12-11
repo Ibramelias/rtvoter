19db43f29a52b47145f48ba7fe35252ac458c246
Ibrameias <ibramdarwish@gmail.com>
2021-12-11 02:44:43 -0500

    get movies from moives db and start to work on get movie's detials

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,21 +1,20 @@
 
-var categories = ['christmas', 'sad', 'happy', 'war', 'lie', 'life'];
-// var categories = ['popular','upcoming', 'top_rated','now_playing'];
-// var imgURL = 'https://image.tmdb.org/t/p/w500';
+// var categories = ['christmas', 'sad', 'happy', 'war', 'lie', 'life'];
+var categories = ['popular','upcoming', 'top_rated','now_playing'];
+var imgURL = "https://image.tmdb.org/t/p/w500";
 
 $(document).ready(function () {
     function getResult(category) {
-        var queryURL = "https://www.omdbapi.com/?s=" + category + "&apikey=trilogy";
-        // var queryURL = "https://api.themoviedb.org/3/movie/"+ category + "?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US&page=1" 
+        // var queryURL = "https://www.omdbapi.com/?s=" + category + "&apikey=trilogy";
+        var queryURL = "https://api.themoviedb.org/3/movie/"+ category + "?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US&page=1";
         return $.get({ url: queryURL })
     }
     categories.forEach(function (category) {
         getResult(category).then(function (response) {
             console.log(response)
-            response.Search.forEach(function (film) {
-                $('#' + category).find('.film-poster').append('<img src=' + film.Poster+ '/>')
-                // $('#' + category).find('.film-poster').append('<p>' + film.release_date+ '</p>')
-
+            response.results.forEach(function (film) {
+                $('#' + category).find('.film-poster').append('<img src=' + imgURL + film.poster_path + '>')
+                // $('#' + category).find('.film-poster').append('<p>' + film.popularity+ '</p>')
             })
         })
     })
