c9275ed8f693c5b792b065130a5c647751d296ec
Ibrameias <ibramdarwish@gmail.com>
2021-12-12 19:38:22 -0500

    display data from api in cards

--- public/js/movies.js
+++ public/js/movies.js
@@ -5,17 +5,29 @@ var categories = ['popular','upcoming', 'top_rated','now_playing'];
 var imgURL = "https://image.tmdb.org/t/p/w154";
 
 $(document).ready(function () {
+    function renderFilmDetails(filmTitle, filmPoster, filmYear) {
+        filmYear = filmYear.slice(0,4);
+        const filmTmeplate = `
+        <div class="film-card">
+            <div class="film-card_poster">
+                <img src=${filmPoster} alt=${filmTitle} class="img-fluid"/>
+            </div>
+            <h2 class="film-card_title">${filmTitle}</h2>
+            <div class="film-card_date">${filmYear}</div>
+        </div>
+        `
+        return filmTmeplate;
+    }
     function getResult(category) {
         // var queryURL = "https://www.omdbapi.com/?s=" + category + "&apikey=trilogy";
         var queryURL = "https://api.themoviedb.org/3/movie/"+ category + "?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US&page=1";
         return $.get({ url: queryURL })
     }
     categories.forEach(function (category) {
-        getResult(category).then(function (response) {
-            console.log(response)
-            response.results.forEach(function (film) {
-                $('#' + category).find('.film-poster').append('<img src=' + imgURL + film.poster_path + '>')
-                // $('#' + category).find('.name').append( "<p>" + film.title  +  "  " +  film.release_date +  "</p>")
+        getResult(category).then(function (films) {
+            films.results.forEach(function (film) {
+                $('#' + category).find('.films-container').append(renderFilmDetails(film.title, imgURL+film.poster_path, film.release_date))
+                // $('#' + category).find('.film-card_poster').append( '<img  src=' + imgURL +  + ">" + "<span>" + "<p class='detalis'>" + film.title + " " + film.release_date + "</P>" +"</span>" )
             })
         })
     })
