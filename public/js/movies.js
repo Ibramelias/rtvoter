31c5ebc25e2e431f94293de98c1caacb826cf058
Ibrameias <ibramdarwish@gmail.com>
2021-12-13 21:50:28 -0500

    trying to create nice swiper for movies

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,12 +1,13 @@
 
 
 // var categories = ['christmas', 'sad', 'happy', 'war', 'lie', 'life'];
-var categories = ['popular','upcoming', 'top_rated','now_playing'];
+var categories = ['popular', 'upcoming', 'top_rated', 'now_playing'];
 var imgURL = "https://image.tmdb.org/t/p/w154";
+
 
 $(document).ready(function () {
     function renderFilmDetails(filmTitle, filmPoster, filmYear) {
-        filmYear = filmYear.slice(0,4);
+        filmYear = filmYear.slice(0, 4);
         const filmTmeplate = `
         <div class="film-card">
             <div class="film-card_poster">
@@ -20,21 +21,28 @@ $(document).ready(function () {
     }
     function getResult(category) {
         // var queryURL = "https://www.omdbapi.com/?s=" + category + "&apikey=trilogy";
-        var queryURL = "https://api.themoviedb.org/3/movie/"+ category + "?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US&page=1";
+        var queryURL = "https://api.themoviedb.org/3/movie/" + category + "?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US&page=1";
         return $.get({ url: queryURL })
     }
     categories.forEach(function (category) {
         getResult(category).then(function (films) {
             films.results.forEach(function (film) {
-                $('#' + category).find('.films-container').append(renderFilmDetails(film.title, imgURL+film.poster_path, film.release_date))
-                // $('#top-ten').find('.films-container').append( renderFilmDetails(film.title, imgURL+film.poster_path, film.release_date))
-               
+                $('#' + category).find('.swiper-slide').append(renderFilmDetails(film.title, imgURL + film.poster_path, film.release_date))
+
             })
         })
     })
-  
+
 })
 
 
-topTen = categories.films.slice(0,10);
-// console.log(topTen);
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
