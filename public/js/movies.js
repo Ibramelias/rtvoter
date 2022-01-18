c9c5efefbcd6b251ef6abeea6a7f70b611915d14
Ibrameias <ibramdarwish@gmail.com>
2022-01-17 20:51:22 -0500

    add data attribute to film card to start to rander modal for more detailes for the movie

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,4 +1,5 @@
 
+
 // var categories = ['christmas', 'sad', 'happy', 'war', 'lie', 'life'];
 var categories = ['popular', 'upcoming', 'top_rated', 'now_playing'];
 var imgURL = "https://image.tmdb.org/t/p/w154";
@@ -6,10 +7,45 @@ var imgURL = "https://image.tmdb.org/t/p/w154";
 
 $(document).ready(function () {
 
-  function renderFilmDetails(filmTitle, filmPoster, filmYear, filmid) {
+
+  function renderModalDetails(filmTitle, filmPoster, filmYear, filmOverView) {
     filmYear = filmYear.slice(0, 4);
+    const modalTmeplate = `
+        <div class="film-car">
+            <div class="film-card_poster">
+                <img src=${filmPoster} alt=${filmTitle} class="img-fluid"/>
+            </div>
+            <h2 class="film-card_title">${filmTitle}</h2>
+            <div class="film-card_date">${filmYear}</div>
+            <p ">${filmOverView}</p>
+        </div>
+        `
+    return modalTmeplate;
+  }
+
+  $(document).on('click', '.film-card', function (filmId) {
+    var filmId = $(this).attr('data-id')
+    console.log(filmId)
+    queryURL = 'https://api.themoviedb.org/3/movie/' + filmId + '?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US';
+    $.ajax({
+      url:queryURL,
+      method:"GET"
+    }).then(function(response){
+      console.log(response)
+      // var MovieTitle = response.title
+      // console.log(MovieTitle)
+      $('.modal').modal("show").find('.modal-body').append(renderModalDetails(response.title, response.year, response.overview))
+    })
+  
+    
+  })
+
+
+  function renderFilmDetails(filmTitle, filmPoster, filmYear, filmId) {
+    filmYear = filmYear.slice(0, 4);
+ 
     const filmTmeplate = `
-        <div class="film-card data-filmid='${filmid}' ">
+        <div class="film-card" data-id=${filmId}>
             <div class="film-card_poster">
                 <img src=${filmPoster} alt=${filmTitle} class="img-fluid"/>
             </div>
@@ -17,18 +53,9 @@ $(document).ready(function () {
             <div class="film-card_date">${filmYear}</div>
         </div>
         `
-        // console.log(filmId)
     return filmTmeplate;
 
   }
-
-  $(document).on('click', '.film-card', function () {
-    filmId = $('.film-card')
-    filmId.last().attr('data-filmid')
-    filmName = $(this).filmId
-    console.log(filmName)
-    $('.modal').modal('show').append()
-  })
 
   function getResult(category) {
     // var queryURL = "https://www.omdbapi.com/?s=" + category + "&apikey=trilogy";
@@ -38,14 +65,15 @@ $(document).ready(function () {
   categories.forEach(function (category) {
     getResult(category).then(function (films) {
       films.results.forEach(function (film) {
-        $('#' + category).find('.films-container').append(renderFilmDetails(film.title, imgURL + film.poster_path, film.release_date, film.id))
-        // console.log(film.id.title)
-       
+        $('#' + category).find('.films-container').prepend(renderFilmDetails(film.title, imgURL + film.poster_path, film.release_date, film.id))
+        // console.log(film.id)
+        // $('.film-card').attr("data-id")
       })
+      // $('.film-card').attr("data-id", filmId)
       // to get the best movies we need to cut the array so we used slice() ///
       var topTen = films.results.slice(0, 3)
       // create map funtion to get the best top ten movies details like name, year and poster 
-      topTen.map(x => { $('#topTen').append(renderFilmDetails(x.title, imgURL + x.poster_path, x.release_date)) });
+      topTen.map(x => { $('#topTen').prepend(renderFilmDetails(x.title, imgURL + x.poster_path, x.release_date)) });
       console.log(films)
     })
   })
@@ -57,7 +85,7 @@ $(document).ready(function () {
       method: 'GET'
     }).then(function (results) {
       results.results.forEach(function (tv) {
-        $("#tvSeries").append(renderFilmDetails(tv.name, imgURL + tv.poster_path, tv.first_air_date))
+        $("#tvSeries").prepend(renderFilmDetails(tv.name, imgURL + tv.poster_path, tv.first_air_date))
       })
     })
   }
