9d517fec083635c418aed85a332040530e67f9d2
Ibrameias <ibramdarwish@gmail.com>
2022-02-05 15:46:21 -0500

    fix the bug.. when the user clicks on get it, the modal will close automatically

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,13 +1,13 @@
 
 
-// var categories = ['christmas', 'sad', 'happy', 'war', 'lie', 'life'];
+// defined variables // 
 var categories = ['popular', 'upcoming', 'top_rated', 'now_playing'];
 var imgURL = "https://image.tmdb.org/t/p/w154";
 
-
+// jquery function // 
 $(document).ready(function () {
 
-
+  // this function is to render Movie model for more details 
   function renderModalDetails(modalTitle, modalPoster, modalYear, modalOverview, modalTagline, modalHomepage, genres) {
     modalYear = modalYear.slice(0, 4);
     const modalTmeplate = `
@@ -20,18 +20,18 @@ $(document).ready(function () {
             </div>
             <div class="modal_date"><strong>Released:</strong> ${modalYear}</div>
             <div class="modal_overview"><strong>Movie overview:</strong> ${modalOverview}</div>
-            <a  href="${modalHomepage}" target="_blank" class="modal_overview"><strong>Get It </strong>
+            <a  href="${modalHomepage}" target="_blank" class="modal_link"><strong>Get It </strong>
         </div>
         `
     return modalTmeplate;
   }
 
-  if(".modal" === false){
-    $('.modal').empty()
-  }
+  // if(".modal" === false){
+  //   $('.modal').empty()
+  // }
 
-
-  $(document).on('click', '.film-card', function() {
+  // this function using ($this) to get the movie we choice and search on themoviedb by filmId
+  $(document).on('click', '.film-card', function () {
     var filmId = $(this).attr('data-id')
     // console.log(filmId)
     queryURL = 'https://api.themoviedb.org/3/movie/' + filmId + '?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US';
@@ -40,33 +40,29 @@ $(document).ready(function () {
       method: "GET"
     }).then(function (res) {
       var genres = res.genres
-      for(var i = 0; i < genres.length; i++){
+      for (var i = 0; i < genres.length; i++) {
         var gen = genres[i].name
         console.log(gen)
       }
-
-      // console.log(genres)
+      // this line is to append what we choice from db 
       $('.modal').modal('show').find('.modal-body').append((renderModalDetails(res.title, imgURL + res.poster_path, res.release_date, res.overview, res.tagline, res.homepage, gen)))
-   
+
     })
   })
 
-  $(document).on('click', '.modal', function(){
+  // this fuction is to make sure that the modal become empty after close 
+  $(document).on('click', '.modal', function () {
     $('.modal-body').empty()
   })
 
-
-  $(document).on('click', '.modal_overview', function(){
-    if('.modal_overview' === false){
-      $('.modal').modal('hidden')
-    }
-    
+  // this functon is to close the modal when the user clicks on get it 
+  $(document).on('click', '.modal_link', function () {
+    $('.modal').modal('hide')
   })
 
-
+  // we created this funtion to render movies in cards
   function renderFilmDetails(filmTitle, filmPoster, filmYear, filmId) {
     filmYear = filmYear.slice(0, 4);
-
     const filmTmeplate = `
         <div class="film-card" data-id=${filmId}>
             <div class="film-card_poster">
@@ -77,25 +73,30 @@ $(document).ready(function () {
         </div>
         `
     return filmTmeplate;
-
   }
 
+  // this function is responsible to get movies from db using  API's and AJAXS 
   function getResult(category) {
-    // var queryURL = "https://www.omdbapi.com/?s=" + category + "&apikey=trilogy";
     var queryURL = "https://api.themoviedb.org/3/movie/" + category + "?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US&page=1";
     return $.get({ url: queryURL })
   }
+
+  // after we get the data, start to loop to get what we need from results 
   categories.forEach(function (category) {
     getResult(category).then(function (films) {
       films.results.forEach(function (film) {
         $('#' + category).find('.films-container').prepend(renderFilmDetails(film.title, imgURL + film.poster_path, film.release_date, film.id))
       })
+
+      // this section is to render only  12 movies from db use slice function to cut what we want from return result
       var topTen = films.results.slice(0, 3)
+
       // create map funtion to get the best top ten movies details like name, year and poster 
       topTen.map(x => { $('#topTen').prepend(renderFilmDetails(x.title, imgURL + x.poster_path, x.release_date)) });
-      console.log(films)
     })
   })
+
+  //  this section is to render TV series 
   function getTvResults() {
     var queryURL1 = 'https://api.themoviedb.org/3/tv/popular?api_key=881f34c3b978ff91294912c9151e1ff4&language=en-US'
     $.ajax({
